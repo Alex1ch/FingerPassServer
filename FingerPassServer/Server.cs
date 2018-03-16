aa54b4ea81f893e613f62f57adbb0bee32e4898f
Alex1ch <lexgorodecky1@gmail.com>
2018-03-16 20:37:23 +0300

    Init commit. Basic device assign

---
+++ FingerPassServer/Server.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.IO;
+using System.Threading;
+using Npgsql;
+using System.Security.Cryptography;
+
+namespace FingerPassServer
+{
+    class Server
+    {
+        static X509Certificate2 serverCertificate = null;
+        static TcpListener listener;
+        static bool alive;
+        string sqlUser = "fingerpassserver";
+        string sqlPass = "matrixislayered";
+        string sqlName = "fingerpassserverdb";
+        static NpgsqlConnectionStringBuilder stringBuilder;
+        Dictionary<IPAddress, SslStream> sslStreamDict = new Dictionary<IPAddress, SslStream>();
+
+        static NpgsqlConnection conn;
+
+        static List<Task> tasks = new List<Task>();
+
+        public static bool Alive { get => alive; set => alive = value; }
+
+        static string CleanUpTasks() {
+            int count=0, active=0, cleaned=0;
+
+            List<Task> toDispose = new List<Task>();
+
+            count=tasks.Count;
+            foreach (Task task in tasks) {
+                if (task.Status == TaskStatus.Running) active++;
+                if (task.Status == TaskStatus.RanToCompletion || task.Status == TaskStatus.Canceled || task.Status == TaskStatus.Faulted) {
+                    toDispose.Add(task);
+                }
+            }
+
+            foreach (Task task in toDispose)
+            {
+                tasks.Remove(task);
+                task.Dispose();
+                cleaned++;
+            }
+            toDispose = null;
+
+            return String.Format("Count: {0}, Active: {1}, Cleaned: {2}", count, active, cleaned);
+        }
+
+
+        Task ListenTcpConnection = new Task(() => {
+        Console.WriteLine("Server is up, waiting for connections");
+
+        while (alive) {
+            TcpClient client = listener.AcceptTcpClient();
+                
+            Console.WriteLine(CleanUpTasks());
+
+                Task ClientHandler = new Task(() => {
+
+                    try
+                    {
+                        SslStreamRW sslStreamRW = new SslStreamRW(client, serverCertificate);
+
+                        Console.WriteLine("TCP Client connected - "+sslStreamRW.Ip+ " -  id: " + sslStreamRW.Id);
+
+                        string message;
+
+                        while (sslStreamRW.Alive&&alive)
+                        {
+                            if (sslStreamRW.ReadString(out message))
+                            {
+                                if(message.Length!=0)
+                                    Console.WriteLine("Recieved message: " + message);
+                                if (message == "<HANDSHAKE>") {
+                                    Handshake(sslStreamRW);
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Exception: {0}", e.Message);
+                        if (e.InnerException != null)
+                        {
+                            Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                        }
+                        return;
+                    }
+                });
+
+                ClientHandler.Start();
+                tasks.Add(ClientHandler);
+
+            }
+        });
+
+
+        static bool Handshake(SslStreamRW sslStreamRw) {
+
+            string login, pass_hash, device_rsa_open_key, server_rsa_private_key, server_rsa_open_key, device_name;
+            if (!sslStreamRw.ReadString(out login)) return false;
+            if (!sslStreamRw.ReadString(out device_name)) return false;
+
+            string salt, rounds, hash, user_id;
+            login = login.Replace("'", "");
+
+            var connection = new NpgsqlConnection(stringBuilder.ToString());
+
+            connection.Open();
+
+            try
+            {
+                string[] splits;
+                splits = new NpgsqlCommand("SELECT password FROM auth_user WHERE username = '" + login + "'", connection).ExecuteScalar().ToString().Split('$');
+                user_id = new NpgsqlCommand("SELECT id FROM auth_user WHERE username = '" + login + "'", connection).ExecuteScalar().ToString();
+                hash = splits[3];
+                salt = splits[2];
+                rounds = splits[1];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in sql request:\n" + e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                if (e.Message == "Object reference not set to an instance of an object.")
+                {
+                    sslStreamRw.Disconnect("User wasn't found");
+                    connection.Close();
+                    return false;
+                }
+                connection.Close(); 
+                sslStreamRw.Disconnect();
+                return false;
+            }
+
+            if (!sslStreamRw.WriteString(salt)) return false;
+            if (!sslStreamRw.WriteString(rounds)) return false;
+            if (!sslStreamRw.ReadString(out pass_hash)) return false;
+
+            if (pass_hash != hash)
+            {
+                sslStreamRw.Disconnect("Wrong password");
+                return false;
+            }
+
+            var rsa = new RSACryptoServiceProvider(2048);
+            server_rsa_private_key = rsa.ToXmlString(true);
+            server_rsa_open_key = rsa.ToXmlString(false);
+
+            if (!sslStreamRw.WriteString(server_rsa_open_key)) return false;
+            if (!sslStreamRw.ReadString(out device_rsa_open_key)) return false;
+
+            Console.WriteLine("Device Name: {3}\nDB Hash: {0}\nRecieved hash: {1}\nRecieved device open key: {2}", hash, pass_hash, device_rsa_open_key, device_name);
+            try
+            {
+                new NpgsqlCommand(String.Format("INSERT INTO devices (device_name,serv_private_key,serv_open_key,device_open_key,user_id) " +
+                    "values ('{0}','{1}','{2}','{3}','{4}');", device_name, server_rsa_private_key, server_rsa_open_key, device_rsa_open_key, user_id), connection).ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in sql insert:\n" + e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                sslStreamRw.Disconnect("Something goes wrong in adding device");
+                return false;
+            }
+            finally {
+                connection.Close();
+            }
+            Console.WriteLine("Handshake successful");
+            //if (!sslStreamRw.ReadString(out device_rsa_open_key)) return false;
+            if (!sslStreamRw.WriteString("<ACCEPTED>")) return false;
+            return true;
+        }
+
+
+        public Server()
+        {
+            sqlUser = "fingerpassserver";
+            sqlPass = "matrixislayered";
+            sqlName = "fingerpassserverdb";
+
+            stringBuilder = new NpgsqlConnectionStringBuilder();
+            stringBuilder.Host = "127.0.0.1";
+            stringBuilder.Port = 5432;
+            stringBuilder.Password = sqlPass;
+            stringBuilder.Username = sqlUser;
+            stringBuilder.Database = sqlName;
+
+            conn = new NpgsqlConnection(stringBuilder.ToString());
+
+            conn.Open();
+
+            new NpgsqlCommand("CREATE TABLE IF NOT EXISTS devices (id SERIAL PRIMARY KEY, device_name NAME NOT NULL," +
+                                                                  "serv_private_key TEXT NOT NULL, serv_open_key TEXT NOT NULL," +
+                                                                  "device_open_key TEXT NOT NULL, user_id INTEGER NOT NULL REFERENCES auth_user (id))", conn).ExecuteNonQuery();
+            new NpgsqlCommand("GRANT ALL ON devices TO fingerpassserver", conn).ExecuteNonQuery();
+
+            conn.Close();
+            alive = true;
+
+            serverCertificate = new X509Certificate2("fingerpass.ru.pfx","metalinmyblood");
+     
+
+            listener = new TcpListener(IPAddress.Any, 6284);
+            listener.Start();
+
+
+            ListenTcpConnection.Start();
+            ListenTcpConnection.Wait();
+        }
+    }
+}
---
+++ FingerPassServer/SslStreamRW.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Text;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Authentication;
+using System.Net;
+
+namespace FingerPassServer
+{
+    class SslStreamRW
+    {
+        TcpClient client;
+        SslStream sslStream;
+
+        long id;
+        string ip;
+        int secTimeOut;
+        string disconnectionReason="Unknown reason";
+        bool alive;
+
+        static long id_pool = 0;
+
+        public string Ip { get => ip; set => ip = value; }
+        public long Id { get => id; set => id = value; }
+        public int SecTimeOut { get => secTimeOut; set => secTimeOut = value; }
+        public string DisconnectionReason { get => disconnectionReason; set => disconnectionReason = value; }
+        public bool Alive { get => alive; set => alive = value; }
+
+        public SslStreamRW(TcpClient _client, string servername)
+        {
+            client = _client;
+            id = id_pool++;
+            ip = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+
+            sslStream = new SslStream(client.GetStream(), true);
+            sslStream.AuthenticateAsClient(servername);
+
+            secTimeOut = 120;
+            alive = true;
+        }
+
+        public SslStreamRW(TcpClient _client, X509Certificate2 cert)
+        {
+            client = _client;
+            id = id_pool++;
+            ip = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+
+            sslStream = new SslStream(client.GetStream(), true);
+            sslStream.AuthenticateAsServer(cert, false, SslProtocols.Tls, true);
+
+            secTimeOut = 120;
+            alive = true;
+        }
+        
+        public void Disconnect()
+        {
+            byte[] zero_length = new byte[2];
+            zero_length[0] = 0;
+            zero_length[1] = 0;
+            sslStream.Write(zero_length, 0, zero_length.Length);
+            sslStream.Write(zero_length, 0, zero_length.Length);
+            Thread.Sleep(300);
+            sslStream.Close();
+            client.Close();
+            alive = false;
+        }
+
+        public void Disconnect(string reason)
+        {
+            byte[] zero_length = new byte[2];
+            zero_length[0] = 0;
+            zero_length[1] = 0;
+            sslStream.Write(zero_length, 0, zero_length.Length);
+            WriteString(reason);
+            Thread.Sleep(300);
+            sslStream.Close();
+            client.Close();
+            alive = false;
+        }
+
+        public void DisconnectNoMessage() {
+            sslStream.Close();
+            client.Close();
+            alive = false;
+        }
+
+        public bool WriteBytes(byte[] message)
+        {
+            if (message.Length == 0)
+            {
+                Disconnect();
+                return false;
+            }
+            byte[] concat = new byte[2 + message.Length];
+            concat[0] = (byte)(message.Length >> 8);
+            concat[1] = (byte)(message.Length);
+            Buffer.BlockCopy(message, 0, concat, 2, message.Length);
+
+            try
+            {
+                sslStream.Write(concat);
+                sslStream.Flush();
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error writing message:\n"+e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Disconnect();
+                return false;
+            }
+        }
+
+        public bool WriteString(string line)
+        {
+            if (line.Length == 0)
+            {
+                Disconnect();
+                return false;
+            }
+            byte[] message = Encoding.UTF8.GetBytes(line);
+            byte[] concat = new byte[2 + message.Length];
+            concat[0] = (byte)(line.Length >> 8);
+            concat[1] = (byte)(line.Length);
+            Buffer.BlockCopy(message, 0, concat, 2, message.Length);
+            try
+            {
+                sslStream.Write(concat);
+                sslStream.Flush();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error writing message:\n" + e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Disconnect();
+                return false;
+            }
+        }
+        
+
+        public bool ReadBytes(out byte[] message)
+        {
+            message = new byte[0];
+            byte[] buffer;
+
+            try
+            {
+                long start = DateTime.Now.Ticks;
+                while (client.Available == 0)
+                {
+                    if (DateTime.Now.Ticks - start > secTimeOut * 10000000)
+                    {
+                        disconnectionReason = "Time Out";
+                        throw new Exception("Disconnected due timeout");
+                    }
+                    Thread.Sleep(50);
+                }
+
+                byte[] lengthBytes = new byte[2];
+
+                sslStream.Read(lengthBytes, 0, 2);
+
+                if (!client.Connected) return false;
+                int length = lengthBytes[0] * 256 + lengthBytes[1];
+                if (length == 0)
+                {
+                    Console.WriteLine("Recieved disconnection signal");
+                    sslStream.Read(lengthBytes, 0, lengthBytes.Length);
+                    length = lengthBytes[0] * 256 + lengthBytes[1];
+                    if (length != 0)
+                    {
+                        byte[] reason = new byte[length];
+                        sslStream.Read(reason, 0, length);
+
+                        StringBuilder reasonData = new StringBuilder();
+
+                        Decoder decoder1 = Encoding.UTF8.GetDecoder();
+                        char[] reason_chars = new char[decoder1.GetCharCount(reason, 0, length)];
+                        decoder1.GetChars(reason, 0, length, reason_chars, 0);
+                        reasonData.Append(reason_chars);
+                        disconnectionReason = reasonData.ToString();
+                    }
+                    DisconnectNoMessage();
+                    return false;
+                }
+
+                buffer = new byte[length];
+
+                sslStream.Read(buffer, 0, length);
+                
+            }
+            catch (Exception e)
+            {   
+                Console.WriteLine("Error reading message:\n" + e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Disconnect();
+                return false;
+            }
+
+            message = buffer;
+            return true;
+        }
+
+        public bool ReadString(out string line)
+        {
+            line = "";
+            StringBuilder messageData = new StringBuilder();
+            int bytes;
+            try
+            {
+                //I know it's very strange code, but ReadTimeout isn't working ;( 
+
+                long start = DateTime.Now.Ticks;
+                while (client.Available == 0)
+                {
+                    if (DateTime.Now.Ticks - start > secTimeOut * 10000000)
+                    {
+                        disconnectionReason = "Time Out";
+                        throw new Exception("Disconnected due timeout");
+                    }
+                    Thread.Sleep(50);
+                }
+
+                byte[] lengthBytes = new byte[2];
+                
+                bytes = sslStream.Read(lengthBytes, 0, lengthBytes.Length);
+                if (!client.Connected) return false;
+                int length = lengthBytes[0] * 256 + lengthBytes[1];
+                if (length == 0)
+                {
+                    Console.WriteLine("Recieved disconnection signal");
+                    bytes = sslStream.Read(lengthBytes, 0, lengthBytes.Length);
+                    length = lengthBytes[0] * 256 + lengthBytes[1];
+                    if (length != 0)
+                    {
+                        byte[] reason = new byte[length];
+                        sslStream.Read(reason, 0, length);
+
+                        StringBuilder reasonData = new StringBuilder();
+
+                        Decoder decoder1 = Encoding.UTF8.GetDecoder();
+                        char[] reason_chars = new char[decoder1.GetCharCount(reason, 0, length)];
+                        decoder1.GetChars(reason, 0, length, reason_chars, 0);
+                        reasonData.Append(reason_chars);
+                        disconnectionReason = reasonData.ToString();
+                    }
+                    DisconnectNoMessage();
+                    return false;
+                }
+
+                byte[] buffer = new byte[length];
+
+                sslStream.Read(buffer, 0, length);
+
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, length)];
+                decoder.GetChars(buffer, 0, length, chars, 0);
+                messageData.Append(chars);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading message:\n" + e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Disconnect();
+                return false;
+            }
+
+            line = messageData.ToString();
+            return true;
+        }
+    }
+}
